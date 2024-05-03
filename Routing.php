c6cf6b552489f465008e2577b4d62049938aee50
mnogiec <nogiecmikolaj@gmail.com>
2024-05-03 15:41:53 +0200

    Refactor routing

---
+++ Routing.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once 'src/controllers/SecurityController.php';
+
+class Router
+{
+  public static $routes;
+
+  public static function get($url, $view)
+  {
+    self::$routes[$url] = $view;
+  }
+
+  public static function post($url, $view)
+  {
+    self::$routes[$url] = $view;
+  }
+
+  public static function run($url)
+  {
+
+    $urlParts = explode("/", $url);
+    $action = $urlParts[0];
+
+    if (!array_key_exists($action, self::$routes)) {
+      ob_start();
+      include 'public/views/not_found.html';
+      print ob_get_clean();
+      return;
+    }
+
+    $controller = self::$routes[$action];
+    $object = new $controller;
+    $action = $action ?: 'index';
+
+    $id = $urlParts[1] ?? '';
+
+    $object->$action($id);
+  }
+}
--- index.php
+++ index.php
@@ -1,41 +1,15 @@
 <?php
 
-require_once 'Database.php';
-require_once 'src/controllers/AppController.php';
-require_once 'src/controllers/SecurityController.php';
-require_once 'src/repository/UserRepository.php';
-require_once 'src/models/User.php';
+require 'Routing.php';
 
-$routing = [
-    'login' => [
-        'controller' => 'SecurityController',
-        'action' => 'login',
-        'access' => []
-    ],
-    'register' => [
-        'controller' => 'SecurityController',
-        'action' => 'register',
-        'access' => []
-    ],
-];
-
-$controller = new AppController();
 
 $path = trim($_SERVER['REQUEST_URI'], '/');
 $path = parse_url($path, PHP_URL_PATH);
-$action = explode("/", $path)[0];
-$action = $action == null ? 'login' : $action;
 
-switch ($action) {
-    case "register":
-    case "login":
-        //TODO check if user is authenticated and has access to system
-        $controllerName = $routing[$action]['controller'];
-        $actionName = $routing[$action]['action'];
-        $controller = new $controllerName;
-        $controller->$actionName();
-        break;
-    default:
-        $controller->render($action);
-        break;
-}
+Router::get('login', 'SecurityController');
+Router::get('register', 'SecurityController');
+
+Router::post('login', 'SecurityController');
+Router::post('register', 'SecurityController');
+
+Router::run($path);
