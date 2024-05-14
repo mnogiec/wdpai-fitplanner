0d7742db86e28c1bb21b6227ff9f501b297ef54f
mnogiec <nogiecmikolaj@gmail.com>
2024-05-14 19:47:19 +0200

    Add routing for workouts and exercises

--- index.php
+++ index.php
@@ -6,15 +6,30 @@ require 'Routing.php';
 $path = trim($_SERVER['REQUEST_URI'], '/');
 $path = parse_url($path, PHP_URL_PATH);
 
+
+// Home
 Router::get('', 'HomeController');
 Router::get('no_access', 'HomeController');
 Router::get('not_found', 'HomeController');
 
+// Auth
 Router::get('login', 'AuthController');
-Router::get('register', 'AuthController');
-
 Router::post('login', 'AuthController');
-Router::post('logout', 'AuthController');
+Router::get('register', 'AuthController');
 Router::post('register', 'AuthController');
+Router::post('logout', 'AuthController');
+
+// Workouts
+Router::get('workouts', 'WorkoutController');
+Router::post('createWorkout', 'WorkoutController');
+Router::patch('updateWorkout', 'WorkoutController');
+Router::delete('deleteWorkout', 'WorkoutController');
+
+// Exercises
+Router::get('exercises_base', 'ExerciseController');
+Router::get('private_exercises', 'ExerciseController');
+Router::post('create_private_exercise', 'ExerciseController');
+Router::patch('update_private_exercise', 'ExerciseController');
+Router::delete('delete_private_exercise', 'ExerciseController');
 
 Router::run($path);
