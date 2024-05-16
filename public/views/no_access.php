8cddade3abc1000f1b829aa632ace0acbdc55d7c
mnogiec <nogiecmikolaj@gmail.com>
2024-05-16 17:31:39 +0200

    Style no_access and not_found pages

--- public/views/no_access.php
+++ public/views/no_access.php
@@ -1,11 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>FitTracker | No access</title>
-  </head>
-  <body>
-    <h1>No access</h1>
-  </body>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" href="public/assets/favicon/favicon.ico" type="image/x-icon">
+  <link rel="stylesheet" type="text/css" href="public/css/base.css">
+  <link rel="stylesheet" type="text/css" href="public/css/extra.css">
+  <title>FitTracker | No access</title>
+</head>
+
+<body>
+  <main class="extra-container">
+    <div class="extra-box">
+      <h1 class="text-4xl font-bold text-primary">No access</h1>
+      <div class="flex-column extra-text-wrapper">
+        <p class="text-gray text-center">Sorry, you are not authorized to see this page.</p>
+        <p class="text-gray text-center">Please visit a page you can access or go back to home page.</p>
+      </div>
+      <a href="/" class="btn extra-btn">Home page</a>
+    </div>
+  </main>
+</body>
+
 </html>
--- public/views/not_found.php
+++ public/views/not_found.php
@@ -1,11 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" href="public/assets/favicon/favicon.ico" type="image/x-icon">
+  <link rel="stylesheet" type="text/css" href="public/css/base.css">
+  <link rel="stylesheet" type="text/css" href="public/css/extra.css">
   <title>FitTracker | Not found</title>
 </head>
+
 <body>
-  <h1>Not found</h1>
+  <main class="extra-container">
+    <div class="extra-box">
+      <h1 class="text-4xl font-bold text-primary">Not found</h1>
+      <div class="flex-column extra-text-wrapper">
+        <p class="text-gray text-center">Sorry, the page you are looking for does not exist.</p>
+        <p class="text-gray text-center">Please make sure the URL is valid or go back to the home page.</p>
+      </div>
+      <a href="/" class="btn extra-btn">Home page</a>
+    </div>
+  </main>
 </body>
+
 </html>
