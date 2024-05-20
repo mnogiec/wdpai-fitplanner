fa73955444e5e7186b4c3738af6b894a3a9f6d28
mnogiec <nogiecmikolaj@gmail.com>
2024-05-20 20:56:07 +0200

    Add extended info about public exercises

--- db.sql
+++ db.sql
@@ -5,7 +5,7 @@
 -- Dumped from database version 16.2 (Debian 16.2-1.pgdg120+2)
 -- Dumped by pg_dump version 16.2
 
--- Started on 2024-05-06 19:32:41 UTC
+-- Started on 2024-05-20 18:55:14 UTC
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -352,37 +352,37 @@ COPY public.exercise_categories (id, name) FROM stdin;
 --
 
 COPY public.exercises (id, name, category_id, description, video_url, creator_id, is_private, image_url) FROM stdin;
-5	Incline Dumbbell Press	5	\N	\N	\N	f	\N
-2	Pec Deck Machine	5	Isolates the chest muscles with controlled movement.	\N	\N	f	\N
-3	Dips	5	Emphasizes the lower chest and triceps when leaning forward.	\N	\N	f	\N
-4	Chest Flys	5	Can be done with dumbbells or a cable machine to enhance chest width and depth.	\N	\N	f	\N
-6	Push-ups	5	Versatile bodyweight exercise that targets the chest along with other upper body muscles.	\N	\N	f	\N
-7	Bench Press	5	Standard exercise using a barbell to build the pectoral muscles.	\N	\N	f	\N
-8	Back Extensions	4	Strengthen the lower back muscles.	\N	\N	f	\N
-9	Face Pulls	4	Improve rear deltoids and upper back muscles.	\N	\N	f	\N
-10	T-Bar Row	4	Compound exercise for mid to upper back development.	\N	\N	f	\N
-11	Lat Pulldowns	4	\N	\N	\N	f	\N
-12	Bent-over Rows	4	Great for horizontal pulling strength, targeting mid-back.	\N	\N	f	\N
-13	Deadlifts	4	\N	\N	\N	f	\N
-14	Pull-ups	4	Strengthen the entire back and biceps with bodyweight.	\N	\N	f	\N
-15	Chin-ups	3	Also works the biceps along with the back.	\N	\N	f	\N
-16	Cable Curl	3	Provides constant tension through the motion.	\N	\N	f	\N
-17	Peacher Curl	3	Stabilizes the arm, increasing isolation during the curl.	\N	\N	f	\N
-18	Concentration Curl	3	Isolates one bicep at a time for focused tension.	\N	\N	f	\N
-19	Hammer Curl	3	Targets the biceps and brachialis with a neutral grip.	\N	\N	f	\N
-20	Barbell Curl	3	Classic exercise for bicep growth.	\N	\N	f	\N
-21	Diamond Push-ups	2	Bodyweight exercise focusing on the triceps and chest.	\N	\N	f	\N
-22	Close-Grip Bench Press	2	\N	\N	\N	f	\N
-23	Overhead Tricep Extension	2	Can be done with a dumbbell or cable to hit different angles.	\N	\N	f	\N
-24	Tricep Pushdown	2	Uses a cable machine for isolation.	\N	\N	f	\N
-25	Skull Crushers	2	Lying tricep extension usually performed with an EZ bar.	\N	\N	f	\N
-26	Tricep Dips	2	Intense bodyweight exercise that targets the triceps.	\N	\N	f	\N
-27	Step-ups	1	Utilizes a bench or platform, good for the thighs and glutes.	\N	\N	f	\N
-28	Calf Raises	1	Targets the calf muscles, can be done on a machine or with free weights.	\N	\N	f	\N
-29	Leg Curls	1	Focuses on the hamstrings.	\N	\N	f	\N
-30	Leg Press	1	Machine-based exercise that allows for heavy lifting with less back strain.	\N	\N	f	\N
-31	Lunges	1	Works each leg individually, promoting balance and muscle symmetry.	\N	\N	f	\N
-32	Squats	1	Fundamental exercise for overall leg development.	\N	\N	f	\N
+10	T-Bar Row	4	Compound exercise for mid to upper back development.	https://www.youtube.com/watch?v=yPis7nlbqdY	\N	f	https://images.ctfassets.net/8urtyqugdt2l/5pziwWANOaPjQS2caLU8vd/83764919b6f35d9b053dd33bcb591fc9/t-bar-row-thumbnail.jpg
+2	Pec Deck Machine	5	Isolates the chest muscles with controlled movement.	https://www.youtube.com/watch?v=O-OBCfyh9Fw	\N	f	https://workouthealthy.com/cdn/shop/files/BS-DPEC-SF_Body-Solid-Pec-Deck-Rear-Delt-Fly-Machine.webp?v=1699486491
+3	Dips	5	Emphasizes the lower chest and triceps when leaning forward.	https://www.youtube.com/watch?v=o2qX3Zb5mvg	\N	f	https://hips.hearstapps.com/hmg-prod/images/dips-1608221119.jpg?resize=980:*
+4	Chest Flys	5	Can be done with dumbbells or a cable machine to enhance chest width and depth.	https://www.youtube.com/watch?v=eozdVDA78K0	\N	f	https://www.garagegymreviews.com/wp-content/uploads/woman-doing-an-incline-chest-fly.jpg
+5	Incline Dumbbell Press	5	\N	https://www.youtube.com/watch?v=8iPEnn-ltC8	\N	f	https://www.dmoose.com/cdn/shop/articles/1_8a79831d-72ad-4d42-aca0-9bc7580f575b.jpg?v=1648826825
+6	Push-ups	5	Versatile bodyweight exercise that targets the chest along with other upper body muscles.	https://www.youtube.com/watch?v=IODxDxX7oi4	\N	f	https://cdn.mos.cms.futurecdn.net/oYDbf5hQAePHEBNZTQMXRA.jpg
+7	Bench Press	5	Standard exercise using a barbell to build the pectoral muscles.	https://www.youtube.com/watch?v=rT7DgCr-3pg	\N	f	https://www.trainheroic.com/wp-content/uploads/2021/09/Bench-press.jpg
+8	Back Extensions	4	Strengthen the lower back muscles.	https://www.youtube.com/watch?v=5_ejbGfdAQE	\N	f	https://hips.hearstapps.com/hmg-prod/images/screen-shot-2022-11-11-at-9-49-18-am-1668183899.png
+9	Face Pulls	4	Improve rear deltoids and upper back muscles.	https://www.youtube.com/watch?v=0Po47vvj9g4	\N	f	https://www.garagegymreviews.com/wp-content/uploads/woman-performing-cable-face-pull.jpg
+11	Lat Pulldowns	4	\N	https://www.youtube.com/watch?v=JGeRYIZdojU	\N	f	https://miro.medium.com/v2/resize:fit:1358/0*7g3xHWvaXcGhd2Ag.jpg
+12	Bent-over Rows	4	Great for horizontal pulling strength, targeting mid-back.	https://www.youtube.com/watch?v=6FZHJGzMFEc	\N	f	https://hips.hearstapps.com/hmg-prod/images/joshua-simpson-kettlebell-vs-dumbbell-kb-bent-over-alternating-row-219-1636665510.jpg?crop=0.529xw:0.767xh;0.288xw,0.198xh&resize=1200:*
+13	Deadlifts	4	\N	https://www.youtube.com/watch?v=AweC3UaM14o	\N	f	https://experiencelife.lifetime.life/wp-content/uploads/2021/08/f2-barbell-deadlift.jpg
+14	Pull-ups	4	Strengthen the entire back and biceps with bodyweight.	https://www.youtube.com/watch?v=aAggnpPyR6E	\N	f	https://i0.wp.com/post.healthline.com/wp-content/uploads/2019/12/pull-up-pullup-gym-1296x728-header-1296x728.jpg?w=1155&h=1528
+15	Chin-ups	3	Also works the biceps along with the back.	https://www.youtube.com/watch?v=8mryJ3w2S78	\N	f	https://media.self.com/photos/5bad13813f15b979ec0368eb/master/pass/woman-doing-chin-up.jpg
+16	Cable Curl	3	Provides constant tension through the motion.	https://www.youtube.com/watch?v=opFVuRi_3b8	\N	f	https://origympersonaltrainercourses.co.uk/files/img_cache/9418/1920_1603357692_bicepcablecurl.JPG?1603357910
+17	Peacher Curl	3	Stabilizes the arm, increasing isolation during the curl.	https://www.youtube.com/watch?v=Ja6ZlIDONac	\N	f	https://prod-ne-cdn-media.puregym.com/media/819541/preacher-curls.png?quality=80
+18	Concentration Curl	3	Isolates one bicep at a time for focused tension.	https://www.youtube.com/watch?v=VMbDQ8PZazY	\N	f	https://cdn.muscleandstrength.com/sites/default/files/seated-concentration-curl.jpg
+19	Hammer Curl	3	Targets the biceps and brachialis with a neutral grip.	https://www.youtube.com/watch?v=RIEMoYL_h1Y	\N	f	https://www.trainheroic.com/wp-content/uploads/2023/02/AdobeStock_417412809-TH-jpg.webp
+20	Barbell Curl	3	Classic exercise for bicep growth.	https://www.youtube.com/watch?v=JnLFSFurrqQ	\N	f	https://mirafit.co.uk/wp/wp-content/uploads/2019/08/fitness-expert-doing-bicep-curls-with-an-ez-cutl-bar-1024x683.jpg
+21	Diamond Push-ups	2	Bodyweight exercise focusing on the triceps and chest.	https://www.youtube.com/watch?v=XtU2VQVuLYs	\N	f	https://res.cloudinary.com/peloton-cycle/image/fetch/f_auto,c_limit,w_3840,q_90/https://images.ctfassets.net/6ilvqec50fal/JdeBsAsNI2XepyM4IDL1U/ef2c96e26f7c3af5bce6db428cd1237f/Screenshot_2024-03-21_at_12.36.05_PM.png
+22	Close-Grip Bench Press	2	\N	https://www.youtube.com/watch?v=XEFDMwmrLAM	\N	f	https://barbend.com/wp-content/uploads/2019/05/Barbend-Featured-Image-1600x900-A-person-doing-a-close-grip-barbell-bench-press-1.jpg
+23	Overhead Tricep Extension	2	Can be done with a dumbbell or cable to hit different angles.	https://www.youtube.com/watch?v=kqidUIf1eJE	\N	f	https://www.dmoose.com/cdn/shop/articles/1_b930cb3e-8dac-4e45-9617-1d7b4594d264.png?v=1646823099
+24	Tricep Pushdown	2	Uses a cable machine for isolation.	https://www.youtube.com/watch?v=LXkCrxn3caQ	\N	f	https://mirafit.co.uk/wp/wp-content/uploads/2022/11/tricep-pushdown-with-Mirafit-Cable-Machine-1-1024x683.jpg
+25	Skull Crushers	2	Lying tricep extension usually performed with an EZ bar.	https://www.youtube.com/watch?v=jO2Jl9eZpXk	\N	f	https://www.dmoose.com/cdn/shop/articles/MicrosoftTeams-image_2_36a5ac4d-8ecb-4b12-91e7-b4f132f5333f.jpg?v=1682782075
+26	Tricep Dips	2	Intense bodyweight exercise that targets the triceps.	https://www.youtube.com/watch?v=oA8Sxv2WeOs	\N	f	https://m.media-amazon.com/images/I/71knyjmUHOL._AC_UF350,350_QL80_.jpg
+27	Step-ups	1	Utilizes a bench or platform, good for the thighs and glutes.	https://www.youtube.com/watch?v=9ZknEYboBOQ	\N	f	https://hips.hearstapps.com/hmg-prod/images/img-2358-jpg-1575476450.jpg
+28	Calf Raises	1	Targets the calf muscles, can be done on a machine or with free weights.	https://www.youtube.com/watch?v=eMTy3qylqnE	\N	f	https://media.gq.com/photos/5a4fe56aefd792474cf8ce17/master/pass/2018-01_GQ_FITNESS-Ask-a-Trainer-Calfs.jpg
+29	Leg Curls	1	Focuses on the hamstrings.	https://www.youtube.com/watch?v=Orxowest56U	\N	f	https://www.bodybuilding.com/images/2016/september/leg-curls-done-light-header-v2-960x540.jpg
+30	Leg Press	1	Machine-based exercise that allows for heavy lifting with less back strain.	https://www.youtube.com/watch?v=yZmx_Ac3880	\N	f	https://ironbullstrength.com/cdn/shop/articles/leg_press_knee_pain.webp?v=1695829075
+32	Squats	1	Fundamental exercise for overall leg development.	https://www.youtube.com/watch?v=nFAscG0XUNY	\N	f	https://steelsupplements.com/cdn/shop/articles/shutterstock_2018381615_1000x.jpg?v=1636630369
+31	Lunges	1	Works each leg individually, promoting balance and muscle symmetry.	\N	\N	f	https://hips.hearstapps.com/hmg-prod/images/muscular-man-training-his-legs-doing-lunges-with-royalty-free-image-1677586874.jpg?crop=0.667xw:1.00xh;0.236xw,0&resize=1200:*
 \.
 
 
@@ -393,7 +393,7 @@ COPY public.exercises (id, name, category_id, description, video_url, creator_id
 --
 
 COPY public.users (id, first_name, last_name, email, password, created_at, is_admin) FROM stdin;
-8	Mikołaj	Nogieć	nogiecmikolaj@gmail.com	$2y$10$19wCmMVejOpKhZd3Vs/JdOXTfH.xw5nlJWlGwdptxhFuqRSbnw97y	2024-05-03 13:27:08.067154+00	f
+11	Mikołaj	Nogieć	nogiecmikolaj@gmail.com	$2y$10$5c8u/eDJM4hqBoOiwof7fOFonTI6lvlYU4ueAWAmsxvhVQMImeVtK	2024-05-14 17:51:35.207926+00	f
 \.
 
 
@@ -449,7 +449,7 @@ SELECT pg_catalog.setval('public.private_exercises_id_seq', 32, true);
 -- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: docker
 --
 
-SELECT pg_catalog.setval('public.users_id_seq', 10, true);
+SELECT pg_catalog.setval('public.users_id_seq', 43, true);
 
 
 --
@@ -578,7 +578,7 @@ ALTER TABLE ONLY public.workouts
     ADD CONSTRAINT workouts_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) NOT VALID;
 
 
--- Completed on 2024-05-06 19:32:41 UTC
+-- Completed on 2024-05-20 18:55:14 UTC
 
 --
 -- PostgreSQL database dump complete
