a4f2d7892b771849b2db1f2fa3e98f298cdee8ab
mnogiec <nogiecmikolaj@gmail.com>
2024-05-30 18:25:36 +0200

    Refactor to be more readable

--- Routing.php
+++ Routing.php
@@ -7,52 +7,64 @@ require_once 'src/controllers/ExerciseController.php';
 
 class Router
 {
-  public static $routes;
+  public static $routes = [
+    'GET' => [],
+    'POST' => [],
+    'PUT' => [],
+    'PATCH' => [],
+    'DELETE' => [],
+  ];
 
-  public static function get($url, $view)
+  public static function get($url, $controller)
   {
-    self::$routes[$url] = $view;
+    self::$routes['GET'][$url] = $controller;
   }
 
-  public static function post($url, $view)
+  public static function post($url, $controller)
   {
-    self::$routes[$url] = $view;
+    self::$routes['POST'][$url] = $controller;
   }
 
-  public static function put($url, $view)
+  public static function put($url, $controller)
   {
-    self::$routes[$url] = $view;
+    self::$routes['PUT'][$url] = $controller;
   }
 
-  public static function patch($url, $view)
+  public static function patch($url, $controller)
   {
-    self::$routes[$url] = $view;
+    self::$routes['PATCH'][$url] = $controller;
   }
 
-  public static function delete($url, $view)
+  public static function delete($url, $controller)
   {
-    self::$routes[$url] = $view;
+    self::$routes['DELETE'][$url] = $controller;
   }
 
   public static function run($url)
   {
-
+    $method = $_SERVER['REQUEST_METHOD'];
     $urlParts = explode("/", $url);
     $action = $urlParts[0];
 
-    if (!array_key_exists($action, self::$routes)) {
+    if (!array_key_exists($action, self::$routes[$method])) {
       ob_start();
       include 'public/views/not_found.php';
       print ob_get_clean();
       return;
     }
 
-    $controller = self::$routes[$action];
+    $controller = self::$routes[$method][$action];
     $object = new $controller;
-    $action = $action ?: 'index';
+    $actionMethod = $action ?: 'index';
+
+    if (!method_exists($object, $actionMethod)) {
+      ob_start();
+      include 'public/views/not_found.php';
+      print ob_get_clean();
+      return;
+    }
 
     $id = $urlParts[1] ?? '';
-
-    $object->$action($id);
+    $object->$actionMethod($id);
   }
 }
