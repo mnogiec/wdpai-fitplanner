8911f1ed82f3fdf6c2c96ddb3011b13f39d0b906
mnogiec <nogiecmikolaj@gmail.com>
2024-05-30 18:23:21 +0200

    Extract consts to define

--- index.php
+++ index.php
@@ -7,11 +7,39 @@ $path = trim($_SERVER['REQUEST_URI'], '/');
 $path = parse_url($path, PHP_URL_PATH);
 
 
-// Home
-Router::get('', 'WorkoutController');
-Router::get('no_access', 'HomeController');
-Router::get('not_found', 'HomeController');
+define('WORKOUT_CONTROLLER', 'WorkoutController');
+define('HOME_CONTROLLER', 'HomeController');
+define('AUTH_CONTROLLER', 'AuthController');
+define('EXERCISE_CONTROLLER', 'ExerciseController');
 
+// Home
+Router::get('', WORKOUT_CONTROLLER);
+Router::get('no_access', HOME_CONTROLLER);
+Router::get('not_found', HOME_CONTROLLER);
+
+// Auth
+Router::get('login', AUTH_CONTROLLER);
+Router::post('login', AUTH_CONTROLLER);
+Router::get('register', AUTH_CONTROLLER);
+Router::post('register', AUTH_CONTROLLER);
+Router::post('logout', AUTH_CONTROLLER);
+
+// Workouts
+Router::get('get_categories', WORKOUT_CONTROLLER);
+Router::get('get_exercises_by_category', WORKOUT_CONTROLLER);
+Router::post('create_workout', WORKOUT_CONTROLLER);
+Router::post('create_workout_day', WORKOUT_CONTROLLER);
+Router::patch('update_workout', WORKOUT_CONTROLLER);
+Router::delete('delete_workout', WORKOUT_CONTROLLER);
+
+// Exercises
+Router::get('exercises_base', EXERCISE_CONTROLLER);
+Router::get('private_exercises', EXERCISE_CONTROLLER);
+Router::get('search_exercises_base', EXERCISE_CONTROLLER);
+Router::get('search_private_exercises', EXERCISE_CONTROLLER);
+Router::post('create_exercise', EXERCISE_CONTROLLER);
+Router::patch('update_exercise', EXERCISE_CONTROLLER);
+Router::delete('delete_exercise', EXERCISE_CONTROLLER);
 // Auth
 Router::get('login', 'AuthController');
 Router::post('login', 'AuthController');
