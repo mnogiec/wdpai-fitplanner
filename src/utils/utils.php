4fe5eb3af09d4ee17a3b2d91c37d929cc0b6f816
mnogiec <nogiecmikolaj@gmail.com>
2024-05-30 18:19:20 +0200

    Create env function to get env variables

--- src/utils/utils.php
+++ src/utils/utils.php
@@ -5,3 +5,15 @@ function redirect($url, $permanent = false)
   header('Location: ' . $url, true, $permanent ? 301 : 302);
   exit();
 }
+
+function env($key, $default = null)
+{
+  $env = parse_ini_file('.env');
+  $value = $env[$key];
+
+  if ($value === null) {
+    return $default;
+  }
+
+  return $value;
+}
